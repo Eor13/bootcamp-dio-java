b3ab72635d7ed8bbbdff8d2f1fc29c74bfd3360b
Everson de Oliveira <eor1309@gmail.com>
2023-02-27 21:59:57 -0300

    New

---
+++ src/aprendendo_sintaxe/operadores/Operadores.java
@@ -0,0 +1,14 @@
+package aprendendo_sintaxe.operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+//        numero = numero +1;
+//        System.out.println( numero ++); //5
+        System.out.println( ++ numero);   //6
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+    }
+}
